b3d34447576970d884a47796fdd23043a87d3e05
brunabarbosa <bruna.barbosa@ccc.ufcg.edu.br>
2017-02-12 16:25:05 -0200

    fazer hist

--- Lab3-Parte1/Lab3-Parte1.R
+++ Lab3-Parte1/Lab3-Parte1.R
@@ -1,4 +1,5 @@
 library(readr)
+library(dplyr)
 #set directory
 setwd("~/AD2/Lab3-Parte1")
 
@@ -6,6 +7,15 @@ setwd("~/AD2/Lab3-Parte1")
 treino_classificacao <- read_csv("~/AD2/Lab3-Parte1/treino_classificacao.csv")
 View(treino_classificacao)
 
+install.packages("plotly")
 
 library(plotly)
 packageVersion('plotly')
+
+p <- plot_ly(x = treino_classificacao$MAT_TUR_ANO, type = 'histogram')
+
+#p <- plot_ly(x = treino_classificacao$MAT_TUR_ANO, type = "histogram")
+p
+
+str(treino_classificacao$MAT_TUR_ANO)
+hist(treino_classificacao$MAT_TUR_ANO + treino_classificacao$EVADIU)
