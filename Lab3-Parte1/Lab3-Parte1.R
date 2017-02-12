7af83ceddc3b4ea56cc89e5ee71fdbb8a0522651
brunabarbosa <bruna.barbosa@ccc.ufcg.edu.br>
2017-02-12 14:45:03 -0200

    lab3-parte1

---
+++ Lab3-Parte1/Lab3-Parte1.R
@@ -0,0 +1,11 @@
+library(readr)
+#set directory
+setwd("~/AD2/Lab3-Parte1")
+
+#import data
+treino_classificacao <- read_csv("~/AD2/Lab3-Parte1/treino_classificacao.csv")
+View(treino_classificacao)
+
+
+library(plotly)
+packageVersion('plotly')
