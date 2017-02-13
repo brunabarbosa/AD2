def0aa636cbd38b7313a34840fa9b63a52ad0b82
Bruna de Oliveira Barbosa 21115494 <brunaob@lcc1-03.lcc.ufcg.edu.br>
2017-02-13 09:16:08 -0300

    histograma adicionado

--- Lab3-Parte1/Lab3-Parte1.R
+++ Lab3-Parte1/Lab3-Parte1.R
@@ -4,18 +4,27 @@ library(dplyr)
 setwd("~/AD2/Lab3-Parte1")
 
 #import data
-treino_classificacao <- read_csv("~/AD2/Lab3-Parte1/treino_classificacao.csv")
+treino_classificacao <- read_csv("treino_classificacao.csv")
 View(treino_classificacao)
 
 install.packages("plotly")
 
+
 library(plotly)
 packageVersion('plotly')
 
-p <- plot_ly(x = treino_classificacao$MAT_TUR_ANO, type = 'histogram')
+#p <- plot_ly(x = treino_classificacao$MAT_TUR_ANO, type = 'histogram')
+#p
 
-#p <- plot_ly(x = treino_classificacao$MAT_TUR_ANO, type = "histogram")
-p
+#str(treino_classificacao$MAT_TUR_ANO)
+#hist(treino_classificacao$MAT_TUR_ANO + treino_classificacao$EVADIU)
 
-str(treino_classificacao$MAT_TUR_ANO)
-hist(treino_classificacao$MAT_TUR_ANO + treino_classificacao$EVADIU)
+
+evadiram <- treino_classificacao[treino_classificacao$EVADIU == TRUE, ]
+naoEvadiram <- treino_classificacao[treino_classificacao$EVADIU == FALSE, ]
+
+#Alunos que evadiram por ano -- ano que mais houve evasao de alunos: 2011
+hist(evadiram$MAT_TUR_ANO)
+
+
+
