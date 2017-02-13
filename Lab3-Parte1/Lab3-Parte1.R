735a1ac4779cf23283a87b6c907eef24a86d310f
brunabarbosa <bruna.barbosa@ccc.ufcg.edu.br>
2017-02-13 09:38:04 -0300

    bar plot

--- Lab3-Parte1/Lab3-Parte1.R
+++ Lab3-Parte1/Lab3-Parte1.R
@@ -1,5 +1,6 @@
 library(readr)
 library(dplyr)
+library(ggplot2)
 #set directory
 setwd("~/AD2/Lab3-Parte1")
 
@@ -26,5 +27,21 @@ naoEvadiram <- treino_classificacao[treino_classificacao$EVADIU == FALSE, ]
 #Alunos que evadiram por ano -- ano que mais houve evasao de alunos: 2011
 hist(evadiram$MAT_TUR_ANO)
 
+#Gere uma visualização da distribuição das classes (número de instâncias de cada classe nos dados);
+#fazer grafico de barras 
+#alunos que evadiram / nao evadiram
+#Tambem pode fazer over time
+
+# Simple Bar Plot
+counts <- table(treino_classificacao$EVADIU)
+barplot(counts, main="Dados de treino",
+        xlab="Evasão") 
+
+
+#Facil de confirmar o que o histograma mostrou, mais pessoas evadiram em 2011
+counts <- table(treino_classificacao$EVADIU, treino_classificacao$MAT_TUR_ANO)
+barplot(counts, main="Dados de treino",
+        xlab="Numero de alunos que evadiram por ano", col=c("darkblue","red"),
+        legend = rownames(counts), beside=TRUE)
 
 
