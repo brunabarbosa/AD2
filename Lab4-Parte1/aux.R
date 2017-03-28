edd070f0792e0f4531bf16326335eaa47ccdf0c5
brunabarbosa <bruna.barbosa@ccc.ufcg.edu.br>
2017-03-28 19:15:10 -0300

    filter incompleto

---
+++ Lab4-Parte1/aux.R
@@ -0,0 +1,62 @@
+setwd("~/Documents/AD2/Lab4-Parte1")
+
+library(readr)
+library(dplyr)
+library(recommenderlab)
+library(reshape2)
+library(ggplot2)
+library(caret)
+
+set.seed(107)
+
+lab4_part1_data <- read_csv("~/Documents/AD2/Lab4-Parte1/lab4_part1_data.csv")
+lab4_part1_data_v2 <- read_csv("~/Documents/AD2/Lab4-Parte1/lab4_part1_data_v2.csv")
+
+dt = sort(sample(nrow(lab4_part1_data), nrow(lab4_part1_data)*.7))
+train<-lab4_part1_data[dt,]
+test<-lab4_part1_data[-dt,]
+
+removeColumns <- c('MAT_NOVA_MATRICULA', 'ALU_ANO_INGRESSO', 'ALU_PERIODO_INGRESSO')
+
+# Convert it as a matrix
+R<-as.matrix( train[which(!(colnames(train)%in%removeColumns))])
+
+
+# Convert R into realRatingMatrix data structure
+#   realRatingMatrix is a recommenderlab sparse-matrix like data-structure
+r <- as(R, "realRatingMatrix")
+r
+
+rec=Recommender(r[1:nrow(r)],method="UBCF", param=list(normalize = "Z-score",method="Cosine",nn=5))
+
+print(rec)
+names(getModel(rec))
+getModel(rec)$nn
+
+#test as matrix
+R<-as.matrix( test[which(!(colnames(test)%in%removeColumns))])
+r <- as(R, "realRatingMatrix")
+
+recom.test <- predict(rec, r[1:nrow(r)], type="ratings")
+recom.test
+#View(as(recom.test, "matrix"))
+
+##Filtrar
+predictedValues <- as(recom.test, "matrix")
+pred.values.df <- cbind(MAT_NOVA_MATRICULA=test$MAT_NOVA_MATRICULA,as.data.frame(predictedValues))
+
+pred.values.df.melt <- melt(pred.values.df,id.vars = "MAT_NOVA_MATRICULA", variable.name = "NOME_DISCIPLINA") %>%
+  mutate(MAT_NOVA_MATRICULA = as.character(MAT_NOVA_MATRICULA),
+         NOME_DISCIPLINA = as.character(NOME_DISCIPLINA))
+
+test.4.periodo <- lab4_part1_data_v2 %>% filter(periodo_relativo == 4)
+
+
+result.4.periodo <- inner_join(pred.values.df.melt,lab4_part1_data_v2)
+
+
+
+
+
+
+
--- Lab4-Parte1/lab4parte1.R
+++ Lab4-Parte1/lab4parte1.R
@@ -3,8 +3,8 @@ setwd("~/Documents/AD2/Lab4-Parte1")
 library(readr)
 library(dplyr)
 
-lab4_part1_train_data <- read_csv("lab4_part1_train_data.csv")
-lab4_part1_test_data <- read_csv("lab4_part1_test_data.csv")
+train <- read_csv("lab4_part1_train_data.csv")
+test <- read_csv("lab4_part1_test_data.csv")
 
 #lab4_part1_data <- read_csv("~/Documents/AD2/Lab4-Parte1/lab4_part1_data.csv")
 #lab4_part1_data_v2 <- read_csv("~/Documents/AD2/Lab4-Parte1/lab4_part1_data_v2.csv")
@@ -12,9 +12,6 @@ lab4_part1_test_data <- read_csv("lab4_part1_test_data.csv")
 library(recommenderlab)
 library(reshape2)
 library(ggplot2)
-
-train <- lab4_part1_train_data
-test <- lab4_part1_test_data
 
 # Using acast to convert above data as follows:
 #       d1  d2   d3   d4
@@ -45,31 +42,6 @@ R<-as.matrix(train.dcast[,-c(1)])
 r <- as(R[,-c(1)], "realRatingMatrix")
 r
 
-# view r in other possible ways
-as(r, "list")     # A list
-as(r, "matrix")   # A sparse matrix
-
-# I can turn it into data-frame
-head(as(r, "data.frame"))
-
-# normalize the rating matrix
-r_m <- normalize(r)
-r_m
-as(r_m, "list")
-
-# Draw an image plot of raw-ratings & normalized ratings
-#  A column represents one specific movie and ratings by users
-#   are shaded.
-#   Note that some items are always rated 'black' by most users
-#    while some items are not rated by many users
-#     On the other hand a few users always give high ratings
-#      as in some cases a series of black dots cut across items
-image(r, main = "Raw Ratings")       
-image(r_m, main = "Normalized Ratings")
-
-
-r_b <- binarize(r, minRating=1)
-as(r_b, "matrix")
 
 # Create a recommender object (model)
 #   Run anyone of the following four code lines.
@@ -80,29 +52,18 @@ as(r_b, "matrix")
 #      Parameter 'method' decides similarity measure
 #        Cosine or Jaccard
 rec=Recommender(r[1:nrow(r)],method="UBCF", param=list(normalize = "Z-score",method="Cosine",nn=5))
-#rec=Recommender(r[1:nrow(r)],method="UBCF", param=list(normalize = "Z-score",method="Jaccard",nn=5))
-#rec=Recommender(r[1:nrow(r)],method="IBCF", param=list(normalize = "Z-score",method="Jaccard"))
-#rec=Recommender(r[1:nrow(r)],method="POPULAR")
 
 # Depending upon your selection, examine what you got
 print(rec)
 names(getModel(rec))
 getModel(rec)$nn
 
-############Create predictions#############################
-# This prediction does not predict movie ratings for test.
-#   But it fills up the user 'X' item matrix so that
-#    for any userid and movieid, I can find predicted rating
-#     dim(r) shows there are 6040 users (rows)
-#      'type' parameter decides whether you want ratings or top-n items
-#         get top-10 recommendations for a user, as:
-#             predict(rec, r[1:nrow(r)], type="topNList", n=10)
-recom <- predict(rec, r[1:nrow(r)], type="ratings")
-recom
-View(as(recom, "matrix"))
+#recom <- predict(rec, r[1:nrow(r)], type="ratings")
+#recom
+#View(as(recom, "matrix"))
 
 
-R<-as.matrix(test.dcast[,-c(1)])
+R <-as.matrix(test.dcast[,-c(1)])
 r <- as(R[,-c(1)], "realRatingMatrix")
 
 recom.test <- predict(rec, r[1:nrow(r)], type="ratings")
@@ -118,6 +79,11 @@ pred.values.df.melt <- melt(pred.values.df,id.vars = "MAT_NOVA_MATRICULA", varia
 
 test.4.periodo <- test %>% filter(periodo_relativo == 4)
 
+
 result.4.periodo <- inner_join(pred.values.df.melt,test)
+
+
+result.4.periodo.merge <- merge(pred.values.df.melt, test)
+ 
 
 
