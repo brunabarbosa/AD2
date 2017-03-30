d48c5a58fbb09be94947b0def2dd2abb0d7932e5
brunabarbosa <bruna.barbosa@ccc.ufcg.edu.br>
2017-03-30 00:01:46 -0300

    fim

--- Lab4-Parte1/lab4parte1.R
+++ Lab4-Parte1/lab4parte1.R
@@ -1,4 +1,4 @@
-setwd("~/Documentos/AD2/Lab4-Parte1")
+setwd("~/Documents/AD2/Lab4-Parte1")
 
 library(readr)
 library(dplyr)
@@ -6,15 +6,14 @@ library(dplyr)
 train <- read_csv("lab4_part1_train_data.csv")
 test <- read_csv("lab4_part1_test_data.csv")
 
-#lab4_part1_data <- read_csv("~/Documents/AD2/Lab4-Parte1/lab4_part1_data.csv")
-#lab4_part1_data_v2 <- read_csv("~/Documents/AD2/Lab4-Parte1/lab4_part1_data_v2.csv")
 
 library(recommenderlab)
 library(reshape2)
 library(ggplot2)
 
-
+#Dataframe sem as notas das displinas que os alunos cursaram no quarto periodo
 test.sem.4.P <- test %>% mutate(MAT_MEDIA_FINAL = ifelse(periodo_relativo == 4, NA, MAT_MEDIA_FINAL)) 
+
 
 remove.inf <- function(x) ifelse(x==-Inf,NA,x)
 
@@ -55,11 +54,11 @@ pred.values.df.melt <- melt(pred.values.df,id.vars = "MAT_NOVA_MATRICULA", varia
 
 test.4.periodo <- test %>% filter(periodo_relativo == 4)
 
-result.4.periodo <- inner_join(pred.values.df.melt,test.4.periodo)
+result.4.periodo <- inner_join(pred.values,test.4.periodo)
 
 
-RMSE(result.4.periodo$value,result.4.periodo$MAT_MEDIA_FINAL) 
+RMSE(result.4.periodo$mediaPred,result.4.periodo$MAT_MEDIA_FINAL) 
 
 #Alterar os metodos de predição
 
-
+length(result.4.periodo$value)
